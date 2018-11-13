6501b7b59981204a727f8d86bac3c55bd0c36510
Rachel Clark <solcode@outlook.com>
2018-11-13 17:39:09 -0500

    Added Queue count command.

---
+++ BeatSaberTwitchIntegration/Commands/GetUserSongCountCommand.cs
@@ -0,0 +1,18 @@
+﻿using AsyncTwitch;
+
+namespace TwitchIntegrationPlugin.Commands
+{
+    public class GetUserSongCountCommand : IrcCommand
+    {
+        public override string[] CommandAlias => new[] {"qcount", "queued"};
+        public override void Run(TwitchMessage msg)
+        {
+            if (!StaticData.UserRequestCount.ContainsKey(msg.Author.DisplayName))
+            {
+                TwitchConnection.Instance.SendChatMessage($"{msg.Author.DisplayName}, you have no songs in the queue.");
+                return;
+            }
+            TwitchConnection.Instance.SendChatMessage($"{msg.Author.DisplayName}, you have {StaticData.UserRequestCount[msg.Author.DisplayName]} songs in queue.");
+        }
+    }
+}
